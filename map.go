0b860e91e7fd6665b2b5e202f597fea25444b7f5
azerothyang <azerothyangg@gmail.com>
2019-12-10 15:58:26 +0800

    getMapKeys2 map[string]string return keys

--- map.go
+++ map.go
@@ -10,3 +10,12 @@ func GetMapKeys(data map[string]interface{}) []string {
 	}
 	return keys
 }
+
+// get map[string]string keys
+func GetMapKeys2(data map[string]string) []string {
+	keys := make([]string, 0)
+	for key := range data {
+		keys = append(keys, key)
+	}
+	return keys
+}
--- map_test.go
+++ map_test.go
@@ -10,10 +10,20 @@ import (
 // get map keys
 func TestGetMapKeys(t *testing.T) {
 	var data = map[string]interface{}{
-		"name": "123",
-		"age": 1,
+		"name":    "123",
+		"age":     1,
 		"profile": "good",
 	}
 	keys := GetMapKeys(data)
+	fmt.Println(keys)
+}
+
+// get map keys
+func TestGetMapKeys2(t *testing.T) {
+	var data = map[string]string{
+		"name":    "123",
+		"profile": "good",
+	}
+	keys := GetMapKeys2(data)
 	fmt.Println(keys)
 }
