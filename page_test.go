a2db9f8aef5692eecb6e706f8630477643fcb0a6
azerothyang <azerothyangg@gmail.com>
2020-02-12 18:45:54 +0800

    add test

--- page_test.go
+++ page_test.go
@@ -7,11 +7,12 @@ import (
 
 //初始化page和pageSize, 默认page为0, pageSize=10, 返回默认为字符串，方便之后查询拼接字符串
 func TestInitPageAndPageSize(t *testing.T) {
-	off, size := InitPageAndPageSize("20", "10")
-	if off != 20 || size != 10 {
+	off, limit := InitPageAndPageSize("20", "10")
+	if off != 200 || limit != 10 {
 		t.Error()
 		return
 	}
 	fmt.Println(InitPageAndPageSize("0.3", "10.2"))
 	fmt.Println(InitPageAndPageSize("-2", "-3"))
+	fmt.Println(InitPageAndPageSize("3", "dasd"))
 }
