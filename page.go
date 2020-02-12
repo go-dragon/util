f4b9435817e4bfd3bc485f86e195de1a7a7bdc29
azerothyang <azerothyangg@gmail.com>
2020-02-12 18:45:34 +0800

    optimize the page utils, now we remain one of the valid number for page or pageSize

--- page.go
+++ page.go
@@ -10,16 +10,16 @@ const (
 )
 
 //初始化page和pageSize, 默认page为0, pageSize=10, 返回默认为字符串，方便之后查询拼接字符串
-func InitPageAndPageSize(page string, pageSize string) (offset int, size int) {
-	p, err := strconv.ParseInt(page, 10, 32)
-	ps, errz := strconv.ParseInt(pageSize, 10, 32)
-	if err != nil || errz != nil {
-		return 0, 10
-	}
+func InitPageAndPageSize(page string, pageSize string) (offset int, limit int) {
+	p, _ := strconv.ParseInt(page, 10, 32)
+	ps, _ := strconv.ParseInt(pageSize, 10, 32)
 	if p < 0 || ps < 0 || ps > MaxPageSize {
 		return 0, 10
 	}
+	if ps == 0 {
+		ps = 10
+	}
 	offset = int(p * ps)
-	size = int(ps)
+	limit = int(ps)
 	return
 }
